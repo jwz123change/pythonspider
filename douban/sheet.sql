ceb37cf267bd6d60f596a9a417aff70b90e29182
wj <1057107380@qq.com>
2019-09-09 10:08:24 +0800

    建表语句

---
+++ douban/sheet.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `douban` (
+  `serial_number` int(11) DEFAULT NULL,
+  `movie_name` varchar(100) DEFAULT NULL,
+  `introduce` varchar(100) DEFAULT NULL,
+  `star` varchar(100) DEFAULT NULL,
+  `evaluate` varchar(200) DEFAULT NULL,
+  `describes` varchar(200) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
